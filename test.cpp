b48d533c4bb4a7e89ce5ce942d52b7653d82a006
SolDavid_76 <david.janusz75@gmail.com>
2023-12-20 11:02:04 +0100

    better

--- test.cpp
+++ test.cpp
@@ -17,6 +17,7 @@ int main(int ac, char** av)
 	socklen_t client_len = sizeof(client_addr);
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	char buffer[1024];
+	std::vector<int> socks;
 
 	// config serv (ipv4/port)
 	server_addr.sin_family = AF_INET;
@@ -38,36 +39,29 @@ int main(int ac, char** av)
 
 	std::cout << "Please wait . . ." << std::endl;
 
-	// struct pollfd fds[1];
-	// fds[0].fd = sock;
-	// fds[0].events = POLLIN;
+	struct pollfd fds[1];
+	fds[0].fd = sock;
+	fds[0].events = POLLIN;
 
-	std::vector<pollfd> poll_fds(5);
-    poll_fds[0].fd = sock;
-    poll_fds[0].events = POLLIN;
-	int newsock = 0;
 	while (1)
 	{
-		// int x = poll(fds, 1, -1);
-		int x = poll(poll_fds.data(), poll_fds.size(), -1);
+		int x = poll(fds, 1, -1);
 
-		if (poll_fds[0].revents & POLLIN)
+		if (fds[0].revents & POLLIN)
 		{
-			if (newsock != 0)
-				newsock = accept(sock, (struct sockaddr*)&client_addr, &client_len);
+			int newsock = accept(sock, (struct sockaddr*)&client_addr, &client_len);
 			if (newsock == -1)
 			{
 				std::cout << "err" << std::endl;
 				continue;
 			}
-			poll_fds[1].fd = newsock;
-			poll_fds[1].events = POLLIN;
-			send(poll_fds.back().fd, "Welcome\n", 9, 0);
+			socks.push_back(newsock);
+			send(socks.back(), "Welcome\n", 9, 0);
 			std::cout << "Someone is connecting . . ." << std::endl;
 
-			for (size_t i = 0; i < poll_fds.size(); i++)
+			for (size_t i = 0; i < socks.size(); i++)
 			{
-				int n = recv(poll_fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+				int n = recv(socks[i], buffer, sizeof(buffer) - 1, 0);
 
 				if (n <= 0)
 				{
@@ -75,8 +69,8 @@ int main(int ac, char** av)
 						std::cout << "Connection closed" << std::endl;
 					else
 						std::cout << "idk" << std::endl;
-					close(poll_fds[i].fd);
-					poll_fds.erase(poll_fds.begin() + i);
+					close(socks[i]);
+					socks.erase(socks.begin() + i);
 					i--;
 					continue;
 				}
